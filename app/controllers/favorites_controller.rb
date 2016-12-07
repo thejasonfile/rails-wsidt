7575296499a7d7caffdbc6cf4ca5d8df5125f24b
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-07 17:06:27 -0500

    refactor to not pass jwt token

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,15 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    user_id = Auth.decode(params["user"]["jwt"])[0]["user_id"]
-    Favorite.find_or_create_by(user_id: user_id, favoritable: Restaurant.find(params["restaurant"]["id"]))
+    Favorite.find_or_create_by(user_id: find_user_id, favoritable: Restaurant.find(params["restaurant"]["id"]))
   end
 
   def destroy
-    user_id = Auth.decode(params["user"]["jwt"])[0]["user_id"]
-    Favorite.find_by(user_id: user_id, favoritable: Restaurant.find(params["restaurant"]["id"])).destroy
+    Favorite.find_by(user_id: find_user_id, favoritable: Restaurant.find(params["restaurant"]["id"])).destroy
+  end
+
+  def find_user_id
+    Auth.decode(request.env["HTTP_AUTHORIZATION"])[0]["user_id"]
   end
 
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,6 @@ class RestaurantsController < ApplicationController
   before_action :authenticate_user
 
   def create
-    user_id = Auth.decode(params["data"]["jwt"])[0]["user_id"]
     results = YelpApi.search(params["data"]["zipcode"])
     render json: {restaurants: results}
   end
