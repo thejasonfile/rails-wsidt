7b138166754faaa3694bd8858f814abef813d3c4
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-07 15:44:43 -0500

    create favorite on checkbox checkk

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,3 +1,12 @@
 class FavoritesController < ApplicationController
-  
+
+  def create
+    user_id = Auth.decode(params["user"]["jwt"])[0]["user_id"]
+    Favorite.create(user_id: user_id, favoritable: Restaurant.find(params["restaurant"]["id"]))
+  end
+
+  def destroy
+    byebug
+  end
+
 end
