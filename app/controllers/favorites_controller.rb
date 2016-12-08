9483805ca0bca7e3ac5de15d706b917fad063c36
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-08 12:46:59 -0500

    favoirte updating fun

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,30 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user
 
+  def index
+    all_user_favorites = Favorite.all.where(user_id: find_user_id)
+    favoritables = all_user_favorites.map do |favorite|
+      favorite.favoritable
+    end.sort!
+    render json: {favorites: favoritables}
+  end
+
   def create
     Favorite.find_or_create_by(user_id: find_user_id, favoritable: Restaurant.find(params["restaurant"]["id"]))
+  end
+
+  def update
+    favorite = Restaurant.find(params[:id]).favorites.find_by(user_id: find_user_id)
+    favorite.update(rating: params[:favorite][:rating], note: params[:favorite][:notes])
+    byebug
+    render json: {favorite: favorite}
   end
 
   def destroy
     Favorite.find_by(user_id: find_user_id, favoritable: Restaurant.find(params["restaurant"]["id"])).destroy
   end
+
+  private
 
   def find_user_id
     Auth.decode(request.env["HTTP_AUTHORIZATION"])[0]["user_id"]
