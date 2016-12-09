0cb82eb43cd93358baee71caf457388da8a5b29e
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-09 16:43:04 -0500

    empty the store upon log out

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,7 +14,7 @@ class FavoritesController < ApplicationController
   end
 
   def update
-    favorite = Restaurant.find(params[:id]).favorites.find_by(user_id: find_user_id)
+    favorite = Favorite.find(params[:id])
     favorite.update(rating: params[:favorite][:rating], note: params[:favorite][:notes])
     # render json: {favorite: favorite}
     all_user_favorites = Favorite.all.where(user_id: find_user_id)
