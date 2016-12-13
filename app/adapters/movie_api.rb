dffd6d7ec415df6e6982240f412ffed008dd7381
jakezappin <jake.zappin@gmail.com>
2016-12-13 13:22:01 -0500

    Fixed Movie Images API Adapter

--- app/adapters/movie_api.rb
+++ app/adapters/movie_api.rb
@@ -9,7 +9,7 @@ class MovieApi
     clnt = HTTPClient.new(proxy)
 
     # apikey = "nr4x9rqbtvdv2475g4z4nnbg"
-    apikey = "gvn5q5azch933mbzsgupxer8"
+    apikey = "2cbbqxx3664hv245va4sjjhm"
     baseUrl = "http://data.tmsapi.com/v1.1/movies/showings?"
     date = Time.now
     today = date.to_s.split(" ")[0]
@@ -19,30 +19,33 @@ class MovieApi
     return @movies
   end
 
-  def getPosterImage(movie_name)
+  def get_image(movie_name)
 
     proxy = ENV['HTTP_PROXY']
     clnt = HTTPClient.new(proxy)
 
-    url_movie_name = movie_name.gsub(" ", "%20")
-    finalURL= "https://api.themoviedb.org/3/search/movie?api_key=2e2815bcd312a818979f639755782396&language=en-US&query=" + url_movie_name + "&page=1&include_adult=false"
-    content = clnt.get_content(finalURL)
+    uri_movie_name = movie_name.gsub(" ", "%20")
+    api_uri= "https://api.themoviedb.org/3/search/movie?api_key=2e2815bcd312a818979f639755782396&language=en-US&query=" + uri_movie_name + "&page=1&include_adult=false"
+    content = clnt.get_content(api_uri)
     movie = JSON.parse(content)
-
-    posterUri = movie['results'][0]['poster_path']
-
-    finalPosterUri = "https://image.tmdb.org/t/p/w500/" + posterUri
-    return finalPosterUri
+    if movie['results'][0] == nil
+      return "NO IMAGE"
+    else
+      poster_uri = movie['results'][0]['poster_path']
+      final_uri= "https://image.tmdb.org/t/p/w500/" + poster_uri
+      return final_uri
+    end
 
   end
 
   def search(zipcode)
     results = callOnConnectAPI(zipcode)
     results = results[0..9]
+    byebug
     results.map do |result|
       if !Movie.find_by({tmsId: result['tmsId']}) && !Movie.find_by({showtimes: result['[showtimes]']})
-        movie_poster_uri = getPosterImage(result['title'])
-        Movie.create({tmsId: result['tmsId'], title: result['title'], description: result['shortDescription'], genres: result['genres'], showtimes: result['showtimes'], image_url: movie_poster_uri})
+        movie_image = get_image(result['title'])
+        Movie.create({tmsId: result['tmsId'], title: result['title'], description: result['shortDescription'], genres: result['genres'], showtimes: result['showtimes'], image_url: movie_image})
       else
         Movie.find_by({tmsId: result['tmsId']})
       end
