4d4bad3f268773cd1c1c3c6e800c70541c218083
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-15 15:56:38 -0500

    add error check for new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true, uniqueness: true
+  validates :password, presence: true
   has_many :favorites
   has_many :restaurants, through: :favorites
 
