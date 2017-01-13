70079a593422e224fdd01c9d2203c5437ff12d3e
Jason Arnold <jason.arnold.00@gmail.com>
2017-01-13 12:57:33 -0500

    user login test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user, only: [:create]
-  
+  skip_before_action :authenticate_user
+
 
   def show
     user = User.find(params[:id])
