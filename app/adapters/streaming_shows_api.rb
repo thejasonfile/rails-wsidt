cdcae7d4dcc1a09dbcec7ac703a627e46e5c33f2
Jason Arnold <jason.arnold.00@gmail.com>
2017-01-14 16:32:41 -0500

    change api keys to environment variables

--- app/adapters/streaming_shows_api.rb
+++ app/adapters/streaming_shows_api.rb
@@ -8,7 +8,7 @@ class StreamingShowsApi
     proxy = ENV['HTTP_PROXY']
     clnt = HTTPClient.new(proxy)
 
-    apikey = "zwpyk7ueg5vce93s98txstwp"
+    apikey = ENV["GUIDEBOX_KEY"]
     finalUrl = "https://api-public.guidebox.com/v1.43/all/rKODOSOrXK285VuQitRZe9bdmMnMBpVs/shows/all/1/25/amazon_prime"
     content = clnt.get_content(finalUrl)
     @streaming_shows = JSON.parse(content)
