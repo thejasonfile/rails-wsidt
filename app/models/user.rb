bc6d72d9678a8c08489b1554affb1f740326e3f1
SluvsP <salomekbg@gmail.com>
2017-01-20 20:22:43 -0500

    Remove has_many restaurants relationship on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :password, presence: true
   has_many :favorites
-  has_many :restaurants, through: :favorites
-
 end
