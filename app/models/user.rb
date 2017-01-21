10f96e1f3b01606fa63f4688bae1193b1eba5e80
SluvsP <salomekbg@gmail.com>
2017-01-20 20:32:27 -0500

    Put back has_many restaurants relationship in User model to test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :password, presence: true
   has_many :favorites
+  has_many :restaurants, through: :favorites
+
 end
